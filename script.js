3350ab0f695928ce5129f6c2302ade11c8e57e12
Chriskybizz <christopherjkenneally@gmail.com>
2020-10-10 16:44:25 +0100

    update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-$(function() {
-$('#form-submit').click(function (e) {
-    e.preventDefault;
-    $('#memory-form input').each(function () {
-        console.log($(this).val())
+$(function () {
+    $('#form-submit').click(function (e) {
+        e.preventDefault;
+        $('#memory-form input').each(function () {
+            console.log($(this).val())
+        });
     });
-});
 });
