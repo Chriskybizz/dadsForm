2e30e5f49c01b398df0cb1c7edf1fbcc18ce53df
Chriskybizz <christopherjkenneally@gmail.com>
2020-10-10 16:41:07 +0100

    Coded added

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-$('input[name="Info"]').click(function (e) {
-    e.preventDefault();
-    $('#form input').each(function () {
-        console.log($(this).val());
-    });
-});
-});
 
-/*var form_data = {
-    "name": document.getElementById("name").value,
-    "relation": document.getElementById("rel").value,
-    "date": document.getElementById("date").value,
-    "memTitle": document.getElementById("haBday").value,
-    "Memory": document.getElementById("mem").value
-
-};
-
-let formSave = () => {
-    console.log(form_data);
-  } //Dunno if this is still needed */
-
-$('#form-submit').click(funtion(e) {
+$(function() {
+$('#form-submit').click(function (e) {
     e.preventDefault;
     $('#memory-form input').each(function () {
         console.log($(this).val())
     });
+});
 });
